6bb8b80482751332075e4738e8464e8a40912881
anton-rakhanov <fantom_nox@hotmail.com>
2016-05-24 22:28:47 +0300

    First homework.

---
+++ CSharpHW/Lection_1/CSharpHomeWork_1/CSharpHomeWork_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomeWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World, ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Anton\n");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Please, press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
