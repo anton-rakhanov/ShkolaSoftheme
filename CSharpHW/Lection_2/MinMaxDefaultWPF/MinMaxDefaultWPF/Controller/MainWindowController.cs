03e9259f6ea29c5a0245b64a508442bbb8dff6a4
Anton <Fantom_Nox@hotmail.com>
2016-05-27 17:45:26 +0300

    Started homework lection_2, WPF MinMaxDefault app

---
+++ CSharpHW/Lection_2/MinMaxDefaultWPF/MinMaxDefaultWPF/Controller/MainWindowController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinMaxDefaultWPF;
+
+namespace MinMaxDefaultWPF.Controller
+{
+    class MainWindowController
+    {
+        private string[] _numTypeArray = new string[11] { "byte", "sbyte","short",
+            "ushort","int","uint","long","ulong","float","double", "decimal"};
+
+        public MainWindowController()
+        {
+        }
+    }
+}
