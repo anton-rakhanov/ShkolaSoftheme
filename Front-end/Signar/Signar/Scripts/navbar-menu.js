c3aba0cd3314899aa48c83e8c297d9fe4c6c9609
anton-rakhanov <fantom_nox@hotmail.com>
2016-06-24 07:52:05 +0300

    Signar UI

---
+++ Front-end/Signar/Signar/Scripts/navbar-menu.js
@@ -0,0 +1,31 @@
+﻿function showSearch() {
+
+    if ($("#searchText").css('display') == 'none') {
+        $("#searchText").css({ 'display': "visible" });
+        $("#searchText").fadeIn(250);
+        
+        $(".navbar-form").css({
+            "border-color": "#C1D6D8",
+            "border-width": "1px",
+            "border-style": "solid"
+        });
+        $(".form-control").focus();
+    } else {
+        $("#searchText").fadeOut(250);
+        $("#searchText").css({ 'display': "none" });
+        $(".navbar-form").css({
+            "border-color": "#C1D6D8",
+            "border-width": "0px",
+            "border-style": "solid"
+        })
+    }
+}
+function showProfileDropMenu() {
+    if ($(".dropdownProfile").css('display') == 'none') {
+        $(".dropdownProfile").css({ 'display': "visible" });
+        $(".dropdownProfile").fadeIn(200);
+    } else {
+        $(".dropdownProfile").fadeOut(200);
+        $(".dropdownProfile").css({ 'display': "none" });
+    }
+}
